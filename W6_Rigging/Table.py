b23f8798273a0a4ec0ed0249538a9bbcf8008a40
Xzep <zephyrxzep@gmail.com>
2023-09-06 02:03:10 +0700

    Update Table.py

--- W6_Rigging/Table.py
+++ W6_Rigging/Table.py
@@ -1,8 +1,16 @@
 import pymel.core as pm
-pm.polyCube(w=1, h=1, d=1, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
-pm.scale([4.302841, 0.465873, 2.411497], r=True)
-pm.move([0, 1.909257, 0], r=True, os=True, wd=True)
 
-pm.polyCube(w=1, h=1, d=1, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
-pm.scale([1, 5.895152, 1], r=True)
-pm.move([0, -0.465873, 0], r=True, os=True, wd=True)
+pm.polyCube(w=4.5, h=0.3, d=3.5, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
+pm.move([0, 1.829189, 0], r=True, os=True, wd=True)
+
+pm.polyCube(w=0.3, h=3, d=0.3, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
+pm.move([1.850, 0.179, -1.350], r=True, os=True, wd=True)
+
+pm.polyCube(w=0.3, h=3, d=0.3, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
+pm.move([-1.850, 0.179, -1.350], r=True, os=True, wd=True)
+
+pm.polyCube(w=0.3, h=3, d=0.3, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
+pm.move([1.850, 0.179, 1.350], r=True, os=True, wd=True)
+
+pm.polyCube(w=0.3, h=3, d=0.3, sx=1, sy=1 ,sz=1 ,ax=[0,1,0], cuv=4, ch=1)
+pm.move([-1.850, 0.179, 1.350], r=True, os=True, wd=True)
